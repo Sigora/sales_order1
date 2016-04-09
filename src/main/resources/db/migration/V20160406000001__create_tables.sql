e1a35787cb348e42173ee08de2caab664a4d2644
starnakin <starnakin@provectus-it.com>
2016-04-09 13:13:22 +0300

    add sql script

---
+++ src/main/resources/db/migration/V20160406000001__create_tables.sql
@@ -0,0 +1,55 @@
+-- Last modification date: 2016-04-06 22:11:14.326
+
+-- tables
+-- Table customer
+CREATE TABLE customer (
+    id bigint  NOT NULL  AUTO_INCREMENT,
+    code varchar(30)  NOT NULL,
+    name varchar(200)  NOT NULL,
+    phone1 bigint  NOT NULL,
+    phone2 bigint  NULL,
+    credit_limit decimal(30,2)  NOT NULL,
+    current_credit decimal(30,2)  NOT NULL,
+    CONSTRAINT customer_pk PRIMARY KEY (id)
+);
+
+-- Table `order`
+CREATE TABLE sale_order (
+    id bigint  NOT NULL  AUTO_INCREMENT,
+    customer_id bigint  NOT NULL,
+    CONSTRAINT order_pk PRIMARY KEY (id)
+);
+
+-- Table product
+CREATE TABLE product (
+    id bigint  NOT NULL  AUTO_INCREMENT,
+    code varchar(30)  NOT NULL,
+    description varchar(200)  NULL,
+    price decimal(30,2)  NOT NULL,
+    quantity int  NOT NULL,
+    CONSTRAINT product_pk PRIMARY KEY (id)
+);
+
+-- Table products_in_order
+CREATE TABLE products_in_order (
+    product_id bigint  NOT NULL,
+    order_id bigint  NOT NULL,
+    CONSTRAINT products_in_order_pk PRIMARY KEY (product_id,order_id)
+);
+
+
+-- foreign keys
+
+ALTER TABLE sale_order ADD CONSTRAINT customer_order_fk FOREIGN KEY (customer_id)
+REFERENCES customer (id);
+
+ALTER TABLE products_in_order ADD CONSTRAINT order_products_in_order_fk FOREIGN KEY (order_id)
+REFERENCES sale_order (id);
+
+ALTER TABLE products_in_order ADD CONSTRAINT product_products_in_order_fk FOREIGN KEY (product_id)
+REFERENCES product (id);
+
+
+
+-- End of file.
+
